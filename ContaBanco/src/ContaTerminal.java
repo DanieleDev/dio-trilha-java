bd9851f10fdebbceee1cae04a9416ddf57193c31
DanieleDev <danieleolivera2012@hotmail.com>
2024-07-12 15:22:57 -0300

    Conta Terminal #1

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor informe seu nome. ");
+        String nome = scan.nextLine();
+
+        System.out.println("Por favor, digite o número da agência. ");
+        String agencia = scan.nextLine();
+
+        System.out.println("Por favor, digite o número da conta. ");
+        int numeroConta = scan.nextInt();
+
+        System.out.println("Por favor, informe qual é seu saldo.");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá, " + nome + ", obrigado por criar uma conta no nosso banco! Sua agência é " +
+        agencia + ", conta " + numeroConta + ". Seu saldo de " + saldo + " já está disponível para saque");
+        
     }
 }
